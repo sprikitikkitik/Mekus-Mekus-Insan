f1434d4ea7473e5ea1a56c180a9befa071e7b375
sprikitikkitik <153047735+sprikitikkitik@users.noreply.github.com>
2023-12-07 16:30:07 +0800

    Add files via upload

---
+++ p.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.subheader("My Webpage")
+st.title("Me, Myself, and I.")
+st.image("https://scontent.fmnl8-2.fna.fbcdn.net/v/t39.30808-6/407835150_389931716798346_1381011220440269177_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=dd5e9f&_nc_eui2=AeE6UFtKg5qJK4VyFP6OZxFRd1zZoZJ_49R3XNmhkn_j1Gqtice9CUKsmEhXCIzuYGIoqCzoOl-uA-_iZuYPL9us&_nc_ohc=in-K7biqz8gAX_Vkxqr&_nc_ht=scontent.fmnl8-2.fna&oh=00_AfCN85SNFeEAGBwnx1O5h5Nwlt_kGeAPRfZv0Q86C9GGTA&oe=657515A1", caption="Hello World", use_column_width=True)
+st.write("Name: Hi! John Mark S. Ravelo")
+st.write("Age: 18")
+st.write("Birth Date: May 24, 2005")
+st.write("Address: Marga, Tubod Surigao Del Norte")
+st.write("  ")
+st.write("Hobby:")
+st.write("-I love Playing Guitar🎸")
+st.write("-I like Playing Online Games🎮")
+st.write("-I love eating Seafoods🦑🦐")
+st.write("-I like Playing Basketball🏀")
+st.write("   ")
+st.write("Yesterday is History, tomorrow is a mystery, but today is a gift. That is why it is called 'Present'.")
+
+
+st.write("Contacts:")
+st.write("-Facebook: https://www.facebook.com/profile.php?id=100073445610712")
+st.write("Instagram: https://l.facebook.com/l.php?u=https%3A%2F%2Finstagram.com%2Fgyfcgg8%3Figshid%3DYTQwZjQ0NmI0OA%253D%253D%26fbclid%3DIwAR0FmHxHU33hHGKHryV1CY-rtsTiiT1D6tuIomdWkQd0pn0oGLsq2EwU5eY&h=AT2sQXWAtkvRTIcKxcosCDvdoK09dfIRD8_8tUKGdy5jd7Zu4Y_WnraxSDCQzPcCjkff6GLJu0h4vDu152LcjuSf59BOfsnhYuffCng-QOs0cXPIBcGJ1pokXr2I3cy9_gYrcA")
